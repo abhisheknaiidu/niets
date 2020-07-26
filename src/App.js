7b3cb38710d8ad2fdb0a773ccefafc867f6e7e8d
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-26 19:28:43 +0530

    Api Functionality Added 👻

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+require("dotenv").config();
+
 
 function App() {
+
+
+  const APP_ID = '2b9b094c';
+  const API_KEY = 'b78c0cd982cbaeeeb49c514faa135af1';
+
+  const [search, setSearch] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  let URL = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=`;
+
+  const fetchRecipes = async () => {
+    await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => setRecipes(data.hits));
+  }
+  const searchRecipe = (e) => {
+    e.preventDefault();
+    URL = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=${search}`;
+    fetchRecipes();
+  }
   return (
     <div>
-      App
+      <form onSubmit={searchRecipe}>
+        <input onChange={ e => setSearch(e.target.value)} />
+        <ul>
+        { recipes.map( recipe => (
+          <li>
+            {recipe.recipe.label}
+          </li>
+        ))}
+        </ul>
+      </form>
     </div>
   );
 }
